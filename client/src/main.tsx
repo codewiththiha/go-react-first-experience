a1b506eecca8557990b21c469236130df763db0b
codewiththiha <kt007441@gmail.com>
2026-01-16 23:51:38 +0630

    force production mode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,11 +5,18 @@ import App from "./App";
 import { Provider } from "@/components/ui/provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+// uncomment this if you are in development stage
 // export const BASEURL =
 // 	import.meta.env.MODE === "development" ? "http://127.0.0.1:4000/api" : "/api";
-export const BASEURL = import.meta.env.PROD
-	? "/api"
-	: "http://127.0.0.1:4000/api";
+
+// fix 2 isn't work either
+// export const BASEURL = import.meta.env.PROD
+// 	? "/api"
+// 	: "http://127.0.0.1:4000/api";
+
+// force production mode
+export const BASEURL = "/api";
+
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
