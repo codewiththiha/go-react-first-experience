0b837121edd6e1b508b2d1f13f7750e50823e624
codewiththiha <kt007441@gmail.com>
2026-01-17 00:09:50 +0630

    debug

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -10,13 +10,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // 	import.meta.env.MODE === "development" ? "http://127.0.0.1:4000/api" : "/api";
 
 // fix 2 isn't work either
-export const BASEURL = import.meta.env.PROD
-	? "/api"
-	: "http://127.0.0.1:4000/api";
+// export const BASEURL = import.meta.env.PROD
+// 	? "/api"
+// 	: "http://127.0.0.1:4000/api";
 
+export const BASEURL = "/api";
 console.log("BASEURL CHECK: ", BASEURL);
 // force production mode
-// export const BASEURL = "/api";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
