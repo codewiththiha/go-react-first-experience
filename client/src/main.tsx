aa5e4aee1b5ec180ef71050135580cce3719ca7c
codewiththiha <kt007441@gmail.com>
2026-01-16 23:46:12 +0630

    fix outdated production mode detection in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,8 +5,11 @@ import App from "./App";
 import { Provider } from "@/components/ui/provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-export const BASEURL =
-	import.meta.env.MODE === "development" ? "http://127.0.0.1:4000/api" : "/api";
+// export const BASEURL =
+// 	import.meta.env.MODE === "development" ? "http://127.0.0.1:4000/api" : "/api";
+export const BASEURL = import.meta.env.PROD
+	? "/api"
+	: "http://127.0.0.1:4000/api";
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
